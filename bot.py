7c1ae436f33b14d68ce7e6aab0d33fed3edcccc3
Raghav Mahajan <1raghavmahajan@gmail.com>
2020-12-14 01:47:27 +0530

    minor updates

--- bot.py
+++ bot.py
@@ -20,16 +20,17 @@ TOKEN = os.environ['TOKEN']
 # change password [password]
 # change default role [default_role] (check if below)
 
+
 class ServerData:
     def __init__(self, np_id=0, def_id=0, channel_id=0, pws='12345'):
         self.np_id = np_id
         self.def_id = def_id
         self.channel_id = channel_id
         self.pw = pws
-    
+
     def __str__(self):
         return f'id: {self.np_id}, def_id: {self.def_id}, channel_id: {self.channel_id}, pw: {self.pw}'
-    
+
     def print(self, guild):
         return f'NP_Role_Name: {guild.get_role(self.np_id)}, Default_Role_Name: {guild.get_role(self.def_id)}, Password_Channel_Name: {guild.get_channel(self.channel_id)}, Password: {self.pw}'
 
@@ -42,6 +43,7 @@ except:
 
 client = discord.Client()
 
+
 async def initialSetup(message):
     g = message.guild
     ms = message.content.split()
@@ -52,27 +54,69 @@ async def initialSetup(message):
     if g.id in storage:
         await message.channel.send(content='Already Initialised')
         return
-    
+
     await message.channel.send(content='Initialising...')
 
-    author_role = message.author.roles[len(message.author.roles)-1]
-    
-    def_role = await g.create_role(name = DEFAULT_ROLE, permissions = author_role.permissions)
+    author_role = message.author.roles[len(message.author.roles) - 1]
 
-    np_role = await g.create_role(name = DEFAULT_NP_ROLE, colour = discord.Colour.darker_grey())
+    def_role = await g.create_role(name=DEFAULT_ROLE,
+                                   permissions=author_role.permissions)
+
+    np_role = await g.create_role(name=DEFAULT_NP_ROLE,
+                                  colour=discord.Colour.darker_grey())
 
     overwrites = {
-        g.default_role: discord.PermissionOverwrite(create_instant_invite = False, manage_channels = False, manage_permissions = False, manage_webhooks = False, add_reactions = False, read_messages = False, send_messages = False, send_tts_messages = False, manage_messages = False, embed_links = False, attach_files = False, read_message_history = False, mention_everyone = False, external_emojis = False),
-        np_role: discord.PermissionOverwrite(create_instant_invite = False, manage_channels = False, manage_permissions = False, manage_webhooks = False, add_reactions = False, read_messages = True, send_messages = True, send_tts_messages = False, manage_messages = False, embed_links = False, attach_files = False, read_message_history = False, mention_everyone = False, external_emojis = False),
-        g.me: discord.PermissionOverwrite(create_instant_invite = True, manage_channels = True, manage_permissions = True, add_reactions = True, read_messages = True, send_messages = True, manage_messages = True, embed_links = True, read_message_history = True)
+        g.default_role:
+        discord.PermissionOverwrite(create_instant_invite=False,
+                                    manage_channels=False,
+                                    manage_permissions=False,
+                                    manage_webhooks=False,
+                                    add_reactions=False,
+                                    read_messages=False,
+                                    send_messages=False,
+                                    send_tts_messages=False,
+                                    manage_messages=False,
+                                    embed_links=False,
+                                    attach_files=False,
+                                    read_message_history=False,
+                                    mention_everyone=False,
+                                    external_emojis=False),
+        np_role:
+        discord.PermissionOverwrite(create_instant_invite=False,
+                                    manage_channels=False,
+                                    manage_permissions=False,
+                                    manage_webhooks=False,
+                                    add_reactions=False,
+                                    read_messages=True,
+                                    send_messages=True,
+                                    send_tts_messages=False,
+                                    manage_messages=False,
+                                    embed_links=False,
+                                    attach_files=False,
+                                    read_message_history=False,
+                                    mention_everyone=False,
+                                    external_emojis=False),
+        g.me:
+        discord.PermissionOverwrite(create_instant_invite=True,
+                                    manage_channels=True,
+                                    manage_permissions=True,
+                                    add_reactions=True,
+                                    read_messages=True,
+                                    send_messages=True,
+                                    manage_messages=True,
+                                    embed_links=True,
+                                    read_message_history=True)
     }
-    pw_channel = await g.create_text_channel(DEFAULT_PASSWORD_CHANNEL_NAME, position=0, overwrites=overwrites)
-    storage[g.id] = ServerData(np_role.id, def_role.id, pw_channel.id, ms[1])        
+    pw_channel = await g.create_text_channel(DEFAULT_PASSWORD_CHANNEL_NAME,
+                                             position=0,
+                                             overwrites=overwrites)
+    storage[g.id] = ServerData(np_role.id, def_role.id, pw_channel.id, ms[1])
 
     await message.delete()
     await message.channel.send(content='Done.')
 
-    pickle.dump(storage, open('serverdata','wb'))
+    pickle.dump(storage, open('serverdata', 'wb'))
+
 
 async def changeSetup(message):
     g = message.guild
@@ -87,19 +131,21 @@ async def changeSetup(message):
         await message.channel.send(content="Password successfully changed")
         await message.delete()
     elif ms[1] == 'default_role':
-        name = message.content[message.content.find('default_role')+13:]
+        name = message.content[message.content.find('default_role') + 13:]
         role = discord.utils.get(g.roles, name=name)
-        if role is not None and g.me.roles[-1]>role:
+        if role is not None and g.me.roles[-1] > role:
             storage[g.id].def_id = role.id
-            await message.channel.send(content=f'Changed Default Role to {role.name}')
+            await message.channel.send(
+                content=f'Changed Default Role to {role.name}')
         else:
             await message.channel.send(content=f'Invalid Role {name}')
     elif ms[1] == 'np_role':
-        name = message.content[message.content.find('np_role')+8:]
+        name = message.content[message.content.find('np_role') + 8:]
         role = discord.utils.get(g.roles, name=name)
-        if role is not None and g.me.roles[-1]>role:
+        if role is not None and g.me.roles[-1] > role:
             storage[g.id].np_id = role.id
-            await message.channel.send(content=f'Changed \'Needs Password\' role to {role.name}')
+            await message.channel.send(
+                content=f'Changed \'Needs Password\' role to {role.name}')
         else:
             await message.channel.send(content=f'Invalid Role {name}')
     elif ms[1] == 'channel' and len(ms) == 3:
@@ -107,13 +153,15 @@ async def changeSetup(message):
         channel = discord.utils.get(g.text_channels, name=name)
         if channel is not None:
             storage[g.id].channel_id = channel.id
-            await message.channel.send(content=f'Changed password channel to {channel.name}')
+            await message.channel.send(
+                content=f'Changed password channel to {channel.name}')
         else:
             await message.channel.send(content='Invalid Channel')
     else:
         return
-    
-    pickle.dump(storage, open('serverdata','wb'))
+
+    pickle.dump(storage, open('serverdata', 'wb'))
+
 
 async def generateHelp(message):
 
@@ -122,7 +170,7 @@ async def generateHelp(message):
     Commands:
     pps!init [password]
         Initialises the bot. Add the role 'Needs Password' to the members who should enter a password before entering.
-    pps!exit
+    !exit
         To manually logout of the server
     pps!change <password/default_role/np_role/channel> [new value]
         password: to change the password
@@ -141,7 +189,7 @@ async def on_ready():
     # guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(f'{client.user} is connected to the following guilds:')
-    
+
     for guild in client.guilds:
         print(f'{guild.name}(id: {guild.id})')
         members = '\n - '.join([member.name for member in guild.members])
@@ -161,7 +209,7 @@ async def on_message(message):
     ms = message.content.split()
 
     # pps!setup
-    if len(ms)>0:
+    if len(ms) > 0:
         if ms[0] == 'pps!init':
             await initialSetup(message)
         elif ms[0] == 'pps!change':
@@ -171,42 +219,48 @@ async def on_message(message):
 
     if g.id in storage:
         if message.channel.id == storage[g.id].channel_id:
-            if(message.content == storage[g.id].pw and discord.utils.get(message.author.roles, id=storage[g.id].np_id) is not None):
-                await message.delete()    
-                def_role = discord.utils.get(message.guild.roles, id=storage[g.id].def_id)
+            if (message.content == storage[g.id].pw
+                    and discord.utils.get(message.author.roles,
+                                          id=storage[g.id].np_id) is not None):
+                await message.delete()
+                def_role = discord.utils.get(message.guild.roles,
+                                             id=storage[g.id].def_id)
                 if def_role is not None:
                     await message.author.add_roles(def_role)
                     print('added role')
-                    await message.channel.send(content=f'You may enter {message.author.display_name}')
+                    await message.channel.send(
+                        content=f'You may enter {message.author.display_name}')
                 else:
-                    await message.channel.send(content='Invalid Configuration', delete_after=40.0)
+                    await message.channel.send(content='Invalid Configuration',
+                                               delete_after=40.0)
             else:
                 await message.delete()
-    
-        elif message.content == 'pps!exit':
-            to_remove = discord.utils.get(message.author.roles, id=storage[g.id].def_id)
-            if(to_remove is not None):
+
+        elif message.content == '!exit':
+            to_remove = discord.utils.get(message.author.roles,
+                                          id=storage[g.id].def_id)
+            if (to_remove is not None):
                 await message.author.remove_roles(to_remove)
                 print(f'Removed role')
             await message.delete()
-    
+
 
 @client.event
 async def on_member_update(before, after):
     g = before.guild
-
     if g.id in storage and before.status != after.status:
         channel = g.get_channel(storage[g.id].channel_id)
-        deleted = await channel.purge(limit=100, check=lambda m: m.author == client.user)
-        if len(deleted)>0:
+        deleted = await channel.purge(limit=100,
+                                      check=lambda m: m.author == client.user)
+        if len(deleted) > 0:
             print('Purged {} message(s)'.format(len(deleted)))
 
-        if after.status == discord.Status.offline and discord.utils.get(after.roles, id=storage[g.id].np_id) is not None: #has np_role
-                print(f'{before.display_name}[{g.name}] went offline')
-                await after.remove_roles(discord.utils.get(g.roles, id=storage[g.id].def_id))
-                print(f'Removed role')
-
+        if after.status == discord.Status.offline and discord.utils.get(
+                after.roles, id=storage[g.id].np_id) is not None:  #has np_role
+            print(f'{before.display_name}[{g.name}] went offline')
+            await after.remove_roles(
+                discord.utils.get(g.roles, id=storage[g.id].def_id))
+            print(f'Removed role')
 
 
 client.run(TOKEN)
-
